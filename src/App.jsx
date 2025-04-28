620c1ef0478c9560578872e18606a406ef6bcc47
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2025-04-28 17:29:55 +0000

    Enhance navigation component with responsive drawer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import { React } from "react";
-import { VStack } from "@chakra-ui/react";
+import { Spacer, VStack } from "@chakra-ui/react";
 import { Provider } from "./context/chakraProvider";
 import { mortuum } from "./themes/mortuum";
 import { glaucous } from "./themes/glaucous";
 import { useArray } from "./hooks/useArray";
 import { PlayGround } from "./components/modules/playGround";
 import { Section } from "./components/layout/section";
-import { Box } from "@chakra-ui/react";
+import { Box, List } from "@chakra-ui/react";
 import { ColorModeButton } from "./components/chakra/color-mode";
 import { Nav } from "./components/layout/nav";
 import { ThemeSwitchButton } from "./components/ui/themeSwitchButton";
@@ -19,20 +19,33 @@ function App() {
   return (
     <Provider theme={theme}>
       <Nav>
-        <ThemeSwitchButton
-          action="previous"
-          handler={chooseTheme}
-        ></ThemeSwitchButton>
+        <Box w="17rem">Yes</Box>
+        <Box>
+          <ThemeSwitchButton
+            action="previous"
+            handler={chooseTheme}
+          ></ThemeSwitchButton>
 
-        <ColorModeButton />
+          <ColorModeButton />
 
-        <ThemeSwitchButton
-          action="next"
-          handler={chooseTheme}
-        ></ThemeSwitchButton>
+          <ThemeSwitchButton
+            action="next"
+            handler={chooseTheme}
+          ></ThemeSwitchButton>
+        </Box>
+        <List.Root
+          flexDirection="row"
+          gap="1rem"
+          justifyContent={{ sm: "center", base: "end" }}
+          variant="plain"
+          w="17rem"
+        >
+          <List.Item>Item 1</List.Item>
+          <List.Item>Item 2</List.Item>
+        </List.Root>
       </Nav>
 
-      <VStack as="main" pt="20">
+      <VStack as="main">
         <PlayGround />
 
         <Section bg="accent.bg" colorPalette="accent">
--- src/components/layout/nav.jsx
+++ src/components/layout/nav.jsx
@@ -1,4 +1,12 @@
-import { HStack } from "@chakra-ui/react";
+import {
+  useBreakpointValue,
+  Drawer,
+  Wrap,
+  Button,
+  Box,
+  VStack,
+} from "@chakra-ui/react";
+import { CiMenuBurger } from "react-icons/ci";
 
 /**
  * A responsive navigation bar component built with Chakra UI's HStack.
@@ -8,22 +16,67 @@ import { HStack } from "@chakra-ui/react";
  *
  * @returns {JSX.Element} The navigation bar component.
  */
-
+const defaultSize = "6rem";
+const defaultMb = "3rem";
 export const Nav = ({ children, ...props }) => {
-  return (
-    <HStack
-      as="nav"
-      zIndex="overlay"
-      position="fixed"
-      w="100%"
-      h="16"
-      px="auto"
-      justify="center"
-      align="center"
-      bg="prim.bg/80"
-      {...props}
-    >
-      {children}
-    </HStack>
+  const isMobile = useBreakpointValue(
+    { base: true, lg: false },
+    { fallback: false },
+  );
+  console.log(isMobile);
+  return isMobile ? (
+    <>
+      <Drawer.Root placement="top">
+        <Drawer.Backdrop bg={props.bg ? props.bg + "/20" : "sec.500/20"} />
+        <Drawer.Trigger asChild>
+          <Button
+            position="fixed"
+            zIndex="sticky"
+            right="1rem"
+            top="1rem"
+            h="3.5rem"
+            w="3.5rem"
+            variant="subtle"
+          >
+            <CiMenuBurger />
+          </Button>
+        </Drawer.Trigger>
+        <Drawer.Positioner>
+          <Drawer.Content
+            bg={props.bg ? props.bg : "accent.bg"}
+            align="center"
+            {...props}
+            h="20%"
+          >
+            <Drawer.Body>
+              <VStack h="100%" justify="center" gap="20%">
+                {children}
+              </VStack>
+            </Drawer.Body>
+          </Drawer.Content>
+        </Drawer.Positioner>
+      </Drawer.Root>
+      <Box mb="3rem"></Box>
+    </>
+  ) : (
+    <>
+      <Wrap
+        gap="5"
+        as="nav"
+        zIndex="overlay"
+        position="fixed"
+        w="100%"
+        px="3rem"
+        py="1.5rem"
+        h={defaultSize}
+        justify={{ base: "center", md: "space-between" }}
+        align="center"
+        bg="accent.bg/80"
+        {...props}
+      >
+        {children}
+      </Wrap>
+      <Box h={defaultSize} mb={defaultMb} {...props} bg="none"></Box>
+    </>
   );
 };
