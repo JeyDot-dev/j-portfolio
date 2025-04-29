e6b6020ec09cc6dbb40954981cf6cdde1df7f735
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2025-04-29 00:28:39 +0000

    Add content context and provider for multilingual support

---
+++ src/context/content.js
@@ -0,0 +1,10 @@
+export const content = {
+  en: {
+    landing: "Hi, I'm Johan - Aspiring Developer",
+    about: "Me like Wapitis",
+  },
+  fr: {
+    landing: "Bonjour, moi c'est Johan, un développeur en herbe",
+    about: "J'aime les wapitis",
+  },
+};
---
+++ src/context/contentProvider.jsx
@@ -0,0 +1,37 @@
+import { useToggle } from "../hooks/useToggle";
+import { createContext, useContext, useEffect } from "react";
+import { content } from "./content";
+
+const LANG_KEY = "preferred-language";
+const ContentContext = createContext();
+function detectLanguage() {
+  const language = navigator.language || navigator.userLanguage;
+  return language.startsWith("fr") ? "fr" : "en";
+}
+function getStorageLanguage() {
+  if (typeof window === "undefined") return detectLanguage();
+  else return localStorage.getItem(LANG_KEY) ?? detectLanguage();
+}
+function updateLanguage(language) {
+  if (typeof window === "undefined") return;
+  localStorage.setItem(LANG_KEY, language);
+}
+
+export function ContentProvider({ children }) {
+  const [language, toggleLanguage, setLanguage] = useToggle("fr", "en");
+  useEffect(() => {
+    setLanguage(getStorageLanguage());
+    console.log(language);
+  }, []);
+  useEffect(() => {
+    updateLanguage(language);
+  }, [language]);
+  return (
+    <ContentContext.Provider
+      value={{ language: language, toggle: toggleLanguage, content }}
+    >
+      {children}
+    </ContentContext.Provider>
+  );
+}
+export const useContentContext = () => useContext(ContentContext);
