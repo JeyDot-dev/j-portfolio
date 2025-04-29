7455f0bc905059719ab4d6631a6abd39076193fa
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2025-04-29 16:20:28 +0000

    Set up initial content structure

---
+++ src/components/context/content.js
@@ -0,0 +1,20 @@
+export const content = {
+  en: {
+    landing: {
+      header: "Hi, I'm Johan - Aspiring Developer",
+      text: "Me like Wapitis",
+    },
+    about: {},
+    projects: {},
+    contact: {},
+  },
+  fr: {
+    landing: {
+      header: "Lorem",
+      text: "Lorem",
+    },
+    about: {},
+    projects: {},
+    contact: {},
+  },
+};
