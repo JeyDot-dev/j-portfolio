5126de248fa48a4e94f57bd1399cc62747dba884
JeyDot-dev <72892603+JeyDot-dev@users.noreply.github.com>
2025-04-29 16:22:22 +0000

    Add 42portraitLogo.svg and update import path for ContentProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,35 @@
 import "./App.css";
 import { React } from "react";
-import { Provider } from "./context/chakraProvider";
+import { Provider } from "./components/context/chakraProvider";
 import { mortuum } from "./themes/mortuum";
 import { glaucous } from "./themes/glaucous";
 import { useArray } from "./hooks/useArray";
 import { PlayGround } from "./components/modules/playGround";
 import { Section } from "./components/layout/section";
-import { Box } from "@chakra-ui/react";
+import { Box, Text, Heading, VStack } from "@chakra-ui/react";
 import { NavBar } from "./components/feature/NavBar";
+import { useContentContext } from "./components/context/contentProvider";
+import PortraitLogo from "./assets/42portraitLogo.svg?react";
 
 const themes = [glaucous, mortuum];
 
 function App() {
   const [theme, chooseTheme] = useArray(themes);
+  const { language, toggleLanguage, content } = useContentContext();
   return (
     <Provider theme={theme}>
       <NavBar themeHandler={chooseTheme} />
       <Box as="main">
-        <Section>
-          <PlayGround />
-        </Section>
-
-        <Section bg="accent.bg" colorPalette="accent">
-          <Box h="66vh">
-            <PlayGround />
-          </Box>
+        <Section id="landing">
+          <VStack>
+            <Box color="prim.solid" mb="3rem">
+              <PortraitLogo />
+            </Box>
+            <Heading size="3xl" color="prim.fg">
+              {content[language].landing.header}
+            </Heading>
+            <Text>{content[language].landing.text}</Text>
+          </VStack>
         </Section>
 
         <Section bg="sec.bg" colorPalette="sec">
